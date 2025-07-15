cca6c034c39ce157cfe685c1dfcb5bbccab3d4fb
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2025-07-15 21:46:42 +0530

    adds assements to the ui

--- frontend/src/pages/teacher/assesments/hooks/useAssesmentsData.jsx
+++ frontend/src/pages/teacher/assesments/hooks/useAssesmentsData.jsx
@@ -1,22 +1,39 @@
 import { EyeIcon } from "lucide-react";
 import { TrashIcon } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  useGetAllAssesmentsQuery,
+  useDeleteAssesmentMutation,
+} from "../../../../store/features/assesments/api";
 
 export const useAssesmentsData = () => {
   const navigate = useNavigate();
+  const { data = [] } = useGetAllAssesmentsQuery();
+  const [triggerDelete] = useDeleteAssesmentMutation();
 
-  const rows = [];
+  const rows = data.map((assessment) => ({
+    ...assessment,
+    template: assessment?.template ? (
+      <Link
+        className="text-blue-500 hover:text-blue-600 cursor-pointer underline"
+        to={`/teacher/templates/${assessment.template._id}`}>
+        {assessment.template.title}
+      </Link>
+    ) : null,
+  }));
 
   const actions = [
     {
       icon: <EyeIcon size={20} className="text-blue-500" />,
       onClick: (row) => {
-        navigate(`/teacher/assesments/${row.id}`);
+        navigate(`/teacher/assesments/${row._id}`);
       },
     },
     {
       icon: <TrashIcon size={20} className="text-red-500" />,
-      onClick: () => {},
+      onClick: (row) => {
+        triggerDelete(row._id);
+      },
     },
   ];
 
