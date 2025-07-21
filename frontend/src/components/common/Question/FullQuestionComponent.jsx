c5a6ad9d68c061132d8acfe80cebbad2f0fef7e2
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2025-07-21 21:36:23 +0530

    add question level

--- frontend/src/components/common/Question/FullQuestionComponent.jsx
+++ frontend/src/components/common/Question/FullQuestionComponent.jsx
@@ -1,28 +1,11 @@
 import React from "react";
-import ChipComponent from "../ChipComponent";
 import Question from "./index.jsx";
-import { assesmentsSelector } from "../../../store/features/assesments/assesmentSlice.js";
-import { useSelector } from "react-redux";
-import { ChipTypes } from "../ChipComponent/types/ChipTypes.js";
-import { questionOptions } from "../../../utils/questionOptions.js";
+import ChipsComponent from "./components/ChipsComponent.jsx";
 
 function FullQuestionComponent({ question, index }) {
-  const { questions = [] } = useSelector(assesmentsSelector);
-
   return (
     <div className="border rounded-lg p-4 bg-white">
-      <div className="flex items-center gap-2 my-2">
-        <ChipComponent
-          text={`Question ${index + 1} of ${questions.length}`}
-          type={ChipTypes.PRIMARY}
-        />
-        <ChipComponent
-          text={`${
-            questionOptions.find((op) => op.id === question.type)?.label
-          }`}
-          type={ChipTypes.SECONDARY}
-        />
-      </div>
+      <ChipsComponent question={question} index={index} />
       <Question question={question} />
     </div>
   );
