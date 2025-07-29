c6d344b3c5a6dfbdc3e1bd015fb58766e64d6f5d
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2025-07-29 21:54:24 +0530

    submit assesment

--- frontend/src/components/Student/TakeAssesment/AssesmentQuestion/NextButton.jsx
+++ frontend/src/components/Student/TakeAssesment/AssesmentQuestion/NextButton.jsx
@@ -6,17 +6,44 @@ import {
   setAssesmentKey,
 } from "../../../../store/features/assesments/assesmentSlice";
 import { useSelector } from "react-redux";
-import { Save } from "lucide-react";
+import { Loader, Save } from "lucide-react";
 import { ChevronRight } from "lucide-react";
+import { useSubmitAssesmentMutation } from "../../../../store/features/submissions/api/submitAssesmentApi";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function NextButton() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { questions, currentQuestionIndex } = useSelector(assesmentsSelector);
+  const { questions, currentQuestionIndex, answers } =
+    useSelector(assesmentsSelector);
+  const [searchParams] = useSearchParams();
+  const submissionId = searchParams.get("submissionId");
   const isLastQuestion = currentQuestionIndex === questions.length - 1;
+  const [triggerSubmitAssesment, { isLoading }] = useSubmitAssesmentMutation();
 
-  const handleNext = () => {
+  const handleNext = async () => {
+    if (isLoading) return;
+
+    if (!submissionId) {
+      console.error("Submission ID not found");
+      return;
+    }
+
     if (isLastQuestion) {
-      console.log("submit");
+      try {
+        await triggerSubmitAssesment({
+          id: submissionId,
+          body: {
+            responses: Object.entries(answers).map(([key, value]) => ({
+              questionId: key,
+              response: JSON.stringify(value),
+            })),
+          },
+        }).unwrap();
+        navigate("/student/submissions");
+      } catch (error) {
+        console.error("Error submitting assesment", error);
+      }
       return;
     }
 
@@ -29,9 +56,15 @@ function NextButton() {
   };
 
   return (
-    <CustomButton onClick={handleNext}>
+    <CustomButton onClick={handleNext} disabled={isLoading}>
       {isLastQuestion ? "Submit" : "Next"}{" "}
-      {isLastQuestion ? <Save size={15} /> : <ChevronRight size={15} />}
+      {isLoading ? (
+        <Loader size={15} className="animate-spin" />
+      ) : isLastQuestion ? (
+        <Save size={15} />
+      ) : (
+        <ChevronRight size={15} />
+      )}
     </CustomButton>
   );
 }
